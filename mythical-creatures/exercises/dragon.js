ab2cf1648748a647ec803397368cc8c9160b0932
Eva Navarrete <evanavarrete07@gmail.com>
2022-03-05 20:50:57 -0700

    Complete test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.eats = 0;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+
+  eat() {
+    this.eats++;
+    if (this.eats < 3) {
+      this.hungry;
+    } else {
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
